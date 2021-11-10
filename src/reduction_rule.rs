070f5ffad162ef4715f83e158ab4c04e72468994
Johannes Meintrup <johannes.meintrup@mni.thm.de>
2021-11-10 13:16:51 +0100

    Improve ReductionRule Interface

--- src/reduction_rule.rs
+++ src/reduction_rule.rs
@@ -1,15 +1,16 @@
 use crate::graph::Graph;
 use bimap::BiMap;
-use std::iter::FromIterator;
 
 trait ReductionRule: From<Graph> {
     fn reduce(self) -> Box<dyn AppliedRule>;
 }
 
 trait AppliedRule {
-    fn old_graph(&self) -> &Graph;
-    fn new_graph(&self) -> &Graph;
-    fn vertex_mapping(&self) -> &BiMap<u32, u32>;
+    fn graph(&self) -> &Graph;
+    fn removed_edges(&self) -> Option<&[(u32, u32)]>;
+    fn removed_vertices(&self) -> Option<&[u32]>;
+    fn vertex_mapping(&self) -> Option<&BiMap<u32, u32>>;
+    fn added_edges(&self) -> Option<&[(u32, u32)]>;
 }
 
 pub struct SelfLoops {
@@ -17,29 +18,30 @@ pub struct SelfLoops {
 }
 
 pub struct SelfLoopsResult {
-    old_graph: Graph,
-    new_graph: Graph,
-    vertex_mapping: BiMap<u32, u32>,
-    removed: Vec<u32>,
+    graph: Graph,
+    removed_edges: Vec<(u32, u32)>,
 }
 
-impl SelfLoopsResult {
-    pub fn removed(&self) -> &[u32] {
-        &self.removed
-    }
-}
 
 impl AppliedRule for SelfLoopsResult {
-    fn old_graph(&self) -> &Graph {
-        &self.old_graph
+    fn graph(&self) -> &Graph {
+        &self.graph
     }
 
-    fn new_graph(&self) -> &Graph {
-        &self.new_graph
+    fn removed_edges(&self) -> Option<&[(u32, u32)]> {
+        Some(&self.removed_edges)
     }
 
-    fn vertex_mapping(&self) -> &BiMap<u32, u32> {
-        &self.vertex_mapping
+    fn removed_vertices(&self) -> Option<&[u32]> {
+        None
+    }
+
+    fn vertex_mapping(&self) -> Option<&BiMap<u32, u32>> {
+        None
+    }
+
+    fn added_edges(&self) -> Option<&[(u32, u32)]> {
+        None
     }
 }
 
@@ -55,16 +57,13 @@ impl ReductionRule for SelfLoops {
     fn reduce(self) -> Box<dyn AppliedRule> {
         let old_graph = self.graph;
         let mut new_graph = old_graph.clone();
-        let removed: Vec<_> = old_graph.vertices().filter(|v| old_graph.has_edge(*v, *v)).collect();
-        for u in &removed {
-            new_graph.remove_edge(*u, *u);
+        let removed_edges: Vec<_> = old_graph.vertices().filter(|v| old_graph.has_edge(*v, *v)).map(|v| (v, v)).collect();
+        for (u, v) in &removed_edges {
+            new_graph.remove_edge(*u, *v);
         }
-        let vertex_mapping = BiMap::from_iter(old_graph.vertices().map(|u| (u, u)));
         Box::new(SelfLoopsResult {
-            old_graph,
-            new_graph,
-            vertex_mapping,
-            removed
+            graph: new_graph,
+            removed_edges
         })
     }
 }
