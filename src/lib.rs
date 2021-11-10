940d0293440a2b7d72ffe0163b10d67f6ab50ac6
Johannes Meintrup <johannes.meintrup@mni.thm.de>
2021-11-10 12:42:15 +0100

    Add logging

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,6 @@ pub mod bitset;
 pub mod graph;
 pub mod io;
 pub mod reduction_rule;
+
+#[cfg(feature = "pace-logging")]
+pub mod log;
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ use std::convert::TryFrom;
 use std::io::{stdin, stdout};
 
 fn main() -> std::io::Result<()> {
+    #[cfg(feature = "pace-logging")]
+    dfvs::log::build_pace_logger();
+
     let stdin = stdin();
     let reader = PaceReader(stdin.lock());
     let graph = Graph::try_from(reader)?;
