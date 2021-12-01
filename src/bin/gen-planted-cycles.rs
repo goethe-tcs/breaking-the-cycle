7df0216a65f0ecd7ef6a3361a727d5ce13772be2
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2021-12-01 17:04:00 +0100

    Add gen-planted-cycles

--- src/bin/gen-planted-cycles.rs
+++ src/bin/gen-planted-cycles.rs
@@ -8,10 +8,13 @@ use rand::SeedableRng;
 use rand_pcg::Pcg64Mcg;
 
 use dfvs::graph::{io::DefaultWriter, AdjArray, Node};
-use dfvs::random_models::gnp::generate_gnp;
+use dfvs::random_models::planted_cycles::generate_planted_cycles;
 
 #[derive(Debug, StructOpt)]
-#[structopt(name = "gen-gnp", about = "Samples a directed Gilbert G(n,p) graph")]
+#[structopt(
+    name = "gen-planted-cycles",
+    about = "Generates a random graph from the planted-cycles model"
+)]
 struct Opt {
     /// Output file. `stdout` if not specified.
     #[structopt(parse(from_os_str))]
@@ -21,9 +24,21 @@ struct Opt {
     #[structopt(short, long)]
     nodes: Node,
 
-    /// Edge probability
-    #[structopt(short, long)]
-    probability: f64,
+    /// Average degree
+    #[structopt(short = "d", long)]
+    avg_deg: f64,
+
+    /// Size of DFVS
+    #[structopt(short = "k", long)]
+    size_dfvs: Node,
+
+    /// Number of cycles
+    #[structopt(short = "c", long)]
+    num_cycles: Node,
+
+    /// Number of nodes in each cycle
+    #[structopt(short = "l", long, default_value = "3")]
+    cycle_len: Node,
 
     /// Seed value
     #[structopt(short, long)]
@@ -33,8 +48,16 @@ struct Opt {
 fn main() -> std::io::Result<()> {
     let opt = Opt::from_args();
 
-    if !(0.0..=1.0).contains(&opt.probability) {
-        error!("Probability has to be in interval [0, 1]");
+    if opt.avg_deg > opt.nodes as f64 / 2.0 {
+        error!("The average degree must not exceed nodes / 2");
+    }
+
+    if opt.nodes < opt.num_cycles * opt.cycle_len + opt.size_dfvs {
+        error!("Need nodes >= num_cycles * cycle_len + size_dfvs");
+    }
+
+    if opt.num_cycles < opt.size_dfvs {
+        error!("Need  num_cycles >= size_dfvs")
     }
 
     let mut gen = match opt.seed {
@@ -44,7 +67,14 @@ fn main() -> std::io::Result<()> {
 
     let writer = DefaultWriter::from_path(opt.output)?;
 
-    let graph: AdjArray = generate_gnp(&mut gen, opt.nodes, opt.probability);
+    let (graph, _): (AdjArray, _) = generate_planted_cycles(
+        &mut gen,
+        opt.nodes,
+        opt.avg_deg,
+        opt.size_dfvs,
+        opt.num_cycles,
+        opt.cycle_len,
+    );
 
     writer.write(&graph)?;
 
