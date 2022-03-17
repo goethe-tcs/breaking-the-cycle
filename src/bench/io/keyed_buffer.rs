aa0b169473596a17e80f22333433085102ff1b1a
Lukas Schwarz <58810022+LukasDavidSchwarz@users.noreply.github.com>
2022-03-17 15:28:07 +0100

    Removed redundant DesignPointBuffer

--- src/bench/io/keyed_buffer.rs
+++ src/bench/io/keyed_buffer.rs
@@ -1,10 +1,10 @@
 use std::collections::HashMap;
 use std::fmt::Display;
 
+/// Used to buffer metrics of a design point
 pub struct KeyedBuffer {
-    //TODO: Use IndexMap for stable element order and remove columns vector
     entries: HashMap<String, String>,
-    columns: Vec<String>, // separate vector is needed for stable order of columns when iterating
+    keys: Vec<String>, // separate vector is needed for stable order of keys when iterating
     is_empty: bool,
 }
 
@@ -12,31 +12,31 @@ impl KeyedBuffer {
     pub fn new() -> Self {
         Self {
             entries: HashMap::new(),
-            columns: vec![],
+            keys: vec![],
             is_empty: true,
         }
     }
 
     /// Write one entry into the buffer
-    pub fn write(&mut self, column: impl Display, value: impl Display) {
-        let column_str = column.to_string();
-        let old_value = self.entries.insert(column_str.clone(), value.to_string());
+    pub fn write(&mut self, key: impl Display, value: impl Display) {
+        let key_str = key.to_string();
+        let old_value = self.entries.insert(key_str.clone(), value.to_string());
         self.is_empty = false;
 
-        // new column
+        // new key
         if old_value.is_none() {
-            self.columns.push(column_str);
+            self.keys.push(key_str);
         }
     }
 
     /// Returns all buffered values
     pub fn flush(&mut self) -> Vec<String> {
         // Replaces all values with empty strings instead of just removing them.
-        // So that the column entries in the map are kept and we can check in O(1) if a column
+        // So that the key entries in the map are kept and we can check in O(1) if a key
         // is new inside of KeyedCsvWriter::write()
         self.is_empty = true;
-        let mut csv_row = Vec::with_capacity(self.columns.len());
-        for key in &self.columns {
+        let mut csv_row = Vec::with_capacity(self.keys.len());
+        for key in &self.keys {
             let old_value = self
                 .entries
                 .insert(key.clone(), String::new())
@@ -47,9 +47,9 @@ impl KeyedBuffer {
         csv_row
     }
 
-    // Returns all columns
-    pub fn get_columns(&self) -> &[String] {
-        &self.columns
+    /// Returns all keys
+    pub fn get_keys(&self) -> &[String] {
+        &self.keys
     }
 
     pub fn is_empty(&self) -> bool {
