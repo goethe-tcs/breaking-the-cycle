66e5ab6557fd01aa73ba2ef2e6bcd9e233aaa58a
Lukas Schwarz <67-LukasSchwarz@users.noreply.gitlab.ae.cs.uni-frankfurt.de>
2022-04-06 18:38:12 +0200

    Add optional dfvs export to dfvs-preprocessing binary

--- create_kernels.sh
+++ create_kernels.sh
@@ -3,15 +3,12 @@
 set -euov pipefail
 cd "$(dirname "$0")"
 
-ARGS='-v --export-sccs data/*/*/*'
-OUTPUT='data/kernels'
+OUTPUT='logs/kernels'
+ARGS="-v --export-sccs --export-dfvs data/*/*/* -o $OUTPUT"
 
 # remove old kernels
 rm -R $OUTPUT || true
 
 # reduce graphs
+mkdir -p $OUTPUT
 cargo +nightly run --features=cli,pace-digest --release --package dfvs --bin dfvs-preprocessing -- $ARGS
-
-# move kernels to separate directory
-mkdir $OUTPUT
-rsync -am --remove-source-files --include='*/' --include='*_kernel.*' --exclude='*' --exclude=$OUTPUT data/ $OUTPUT
