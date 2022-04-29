b549758fb4c188723c989de73270d0c10ac5b00d
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2022-04-29 10:27:53 +0200

    Update run_exact.sh Deleted regression.py

--- run_exact.sh
+++ run_exact.sh
@@ -7,15 +7,9 @@ rm -f DIR/*.solution
 
 cargo b --all-features --release --bin dfvs-cli
 
-#for f in $DIR/e_*.metis
-#do
-#  echo "$f ------------------------------------"
-#  target/release/dfvs-cli -vv -m exact -i $f -o $f.solution | tee $f.log
-#done
-
 ls -1 $DIR/e_*.metis | \
   parallel --timeout 600 \
-    target/release/dfvs-cli -m exact -i {} -o {.}.solution
+    target/release/dfvs-cli -vv -m exact -i {} -o {.}.solution
 
 for f in $DIR/*.solution
 do
