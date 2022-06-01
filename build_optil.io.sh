12945d01612a2172c43a54c6191bceec7b50569d
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2022-06-01 14:03:17 +0200

    Update README.md

--- build_optil.io.sh
+++ build_optil.io.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-export RUSTFLAGS='-C target-feature=+crt-static -C target-cpu=haswell'
-BUILD_CMD="cargo build --release --target x86_64-unknown-linux-gnu"
+export RUSTFLAGS='-C target-feature=+crt-static -C target-cpu=haswell -C target-feature=+bmi2,+bmi1'
+BUILD_CMD="cargo +nightly build --release --target x86_64-unknown-linux-gnu"
 
 $BUILD_CMD --bin optil_exact
 $BUILD_CMD --bin optil_heuristic --features="signal-handling"
